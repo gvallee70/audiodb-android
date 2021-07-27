8702598f36424e7c0787e3402849546d37244dc7
Gwendal Vallée <gwendal.vallee@hotmail.fr>
2021-07-27 06:22:27 +0200

    Added favorites UI

--- app/src/main/java/com/artfelt/theaudiodb/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/artfelt/theaudiodb/ui/favorites/FavoritesFragment.kt
@@ -6,36 +6,55 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.artfelt.theaudiodb.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class FavoritesFragment : Fragment() {
 
-    private lateinit var favoritesViewModel: FavoritesViewModel
-
     private lateinit var favoritesTitleTextView: TextView
+    private lateinit var favoritesSubtitleTextView: TextView
+    private lateinit var mArtistTitleTextView: TextView
+    private lateinit var mArtistRecyclerView: RecyclerView
+    private lateinit var mAlbumTitleTextView: TextView
+    private lateinit var mAlbumRecyclerView: RecyclerView
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        favoritesViewModel =
-                ViewModelProvider(this).get(FavoritesViewModel::class.java)
-        val view = inflater.inflate(R.layout.fragment_favorites, container, false)
-        favoritesTitleTextView =  view.findViewById(R.id.textView_title_favorites)
 
-        /*favoritesViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })*/
+        return inflater.inflate(R.layout.fragment_favorites, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var bottomNavigationView = activity?.findViewById<BottomNavigationView>(R.id.nav_view)
+
+        bottomNavigationView?.visibility = View.VISIBLE
+
+        favoritesTitleTextView = view.findViewById(R.id.textView_title_favorites)
+        favoritesSubtitleTextView = view.findViewById(R.id.textView_favorites_subtitle)
+        mArtistTitleTextView = view.findViewById(R.id.textView_artist_title)
+        mArtistRecyclerView = view.findViewById(R.id.recyclerView_artists)
+        mAlbumTitleTextView = view.findViewById(R.id.textView_albums_title)
+        mAlbumRecyclerView = view.findViewById(R.id.recyclerView_albums)
+
         initView()
 
-        return view
     }
 
     private fun initView() {
         initFavoritesTitle()
+        initFavoritesSubtitle()
+        initArtistsTitle()
+        initAlbumsTitle()
     }
 
     private fun initFavoritesTitle() {
@@ -43,4 +62,17 @@ class FavoritesFragment : Fragment() {
         favoritesTitleTextView.typeface = Typeface.DEFAULT_BOLD
         favoritesTitleTextView.text = getString(R.string.title_favorites)
     }
+
+    private fun initFavoritesSubtitle() {
+        favoritesSubtitleTextView.text = getString(R.string.label_my_favorites)
+    }
+
+    private fun initArtistsTitle() {
+        mArtistTitleTextView.text = getString(R.string.label_artists)
+    }
+
+    private fun initAlbumsTitle() {
+        mAlbumTitleTextView.text = getString(R.string.label_albums)
+    }
+
 }
